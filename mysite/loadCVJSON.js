9527a85d5ee3847e72ecd376ede8de4c3099cd2f
Hampus Karlsson <33913617+hahmpus@users.noreply.github.com>
2018-02-14 20:07:13 +0100

    Added the site
    Added the whole current and up to date site. WIP

---
+++ mysite/loadCVJSON.js
@@ -0,0 +1,61 @@
+/*object containg personal information*/
+var persObj = {"Firstname":"Hampus", "Surname":"Karlsson", "age":30};
+
+/*object containing a pitch*/
+var pitchObj = {
+	"Pitch":"I am an effective and organized person. Once I am in a project I tend to dedicate all my energy to it, whether it’s a game or any other type of challenge. I like to confront obstacles and I try to learn as much as I can in any given situation. For better or worse, I do care a lot about other people’s opinions."
+};
+
+/*object containing all contact information*/
+var contObj = {"contact":[
+   
+    {	"Street":"Vallgatan 2B", 
+    	"City":"Skara", 
+    	"Zip":"53236", 
+    	"GitHub":"Hahmpus" 
+	},
+]}
+
+/*object containing all educational information*/
+var eduObj = {"educations":[
+
+    {	"School":"Katrineholms Tekniska College", 
+    	"Abbrevation":"KTC", 
+    	"Start":"2010", 
+    	"End":"2013", 
+    	"Description":"A three year long secondary education with focus on IT called Information and Technology. A deeper focus on design was chosen. I learned how important a careful design of a product can be, and how different designs can change the outcome drastically. Essentially, how to make the right product for the right customer. I learned how to use various methods and tools to accomplish this." 
+    },
+
+    { 	"School":"Linköpings Universitet", 
+    	"Abbrevation":"LIU", 
+    	"Start":"2013", 
+    	"End":"2013", 
+    	"Description":"A weeklong course in logistics. Here I got to learn about all the factors that can be included in particular processes such as delivery and storing of a product, and how to optimize these processes.	I ended this course with a diploma for ”Most Efficient Sales” in an exercise about selling beads to a customer." 
+    },
+
+    { 	"School":"Högskolan i Skövde", 
+    	"Abbrevation":"HiS", 
+    	"Start":"2014", 
+    	"End":"2017", 
+    	"Description":"A very broad education called ”Web programming” with a focus on the web and it’s technologies. I got to learn different programming languages and how to apply them in different levels of an IT-system (backend and frontend). I learned how to troubleshoot, test, optimize and maintain an IT-system. Of course, I learned a fair bit about security in, well, everything: safe code, safe design, safe location of operations etc. I participated in many solo- and group projects designed with operations and organizations in mind." 
+    }
+]}
+
+/*obejct containing all lingual information*/
+var langObj = {"Languages":[{"Language":"Swedish","Language":"English"},
+]}
+
+/*object containing information about merits*/
+var meritObj = {"merits":[
+
+    {	"Subject":"Programming",
+    	"Merit":"My strength lies with the web side of things. I’m most comfortable in JavaScript but I do know how to use HTML, CSS, PHP, XML and so on. I also know how to use Java and C, but I am not as fluent in these." 
+    },
+
+    { 	"Subject":"IT in Practice",
+    	"Merit":"Thanks to my education I know how IT actually is used by companies, and how to try to customize a system. Some courses in this area is CMS and service, Information Security, Software engineering and IT in organizations." 
+    }
+]}
+
+var x = eduObj.educations[0];
+console.log(x);
